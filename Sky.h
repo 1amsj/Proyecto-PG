5ce9ff1162a99d8c29862d49f5c78686599c29cb
1amsj <149028405+1amsj@users.noreply.github.com>
2025-05-25 20:51:54 -0600

    Avance 1

---
+++ Sky.h
@@ -0,0 +1,11 @@
+#ifndef SKY_CLASS_H
+#define SKY_CLASS_H
+
+#include <GLFW/glfw3.h>
+
+class Sky {
+
+};
+
+#endif
+#pragma once
--- main.cpp
+++ main.cpp
@@ -1,2 +1,68 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <vector>
+#include "Sky.h"
+
+// para cambiar el tamaño de la pantalla
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+
+}
+
+// esc para cerrar la pantalla
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1000, 800, "Ciclo Solar", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Se falló en la creación de la ventana de GLFW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Se falló en la creación de la ventana de GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 1000, 800);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//centrar la pantalla
+
+	GLFWmonitor* pantalla = glfwGetPrimaryMonitor();
+	const GLFWvidmode* mode = glfwGetVideoMode(pantalla);
+	if (mode) {
+		int sceenWidth = mode->width;
+		int screenHeight = mode->height;
+		int posX = (sceenWidth - 1000) / 2;
+		int posY = (screenHeight - 800) / 2;
+		glfwSetWindowPos(window, posX, posY);
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		glClearColor(0.6, 0.9, 1.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
