526e325b8e48850734940a8c7def3162ec7dca86
1amsj <149028405+1amsj@users.noreply.github.com>
2025-06-16 16:09:31 -0600

    Add files via upload

---
+++ Archivos de encabezado/Presentacion.h
@@ -0,0 +1,18 @@
+#ifndef PRESENTACION_CLASS_H  
+#define PRESENTACION_CLASS_H  
+
+#include <GLFW/glfw3.h>  
+#include <iostream>
+
+class presentacion {  
+public:  
+	static void mouse_callback(GLFWwindow* window, int button, int action, int mods);  
+	void drawButton();  
+	void drawStars();  
+	
+
+};  
+
+#endif // !PRESENTACION_CLASS_H  
+
+#pragma once
---
+++ Archivos de encabezado/Sky.h
@@ -0,0 +1,12 @@
+#ifndef SKY_CLASS_H
+#define SKY_CLASS_H
+
+#include <GLFW/glfw3.h>
+
+class Sky {
+public:
+	void drawSky();
+};
+
+#endif
+#pragma once
